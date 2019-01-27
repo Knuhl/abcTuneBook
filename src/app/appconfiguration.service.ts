4dd7938bfa228fcc052856a104496c35ad8178d7
Dave <david.emperle@gmail.com>
2019-01-27 10:57:33 +0100

    added git --assume-unchanged tip for app-config

--- src/app/appconfiguration.service.ts
+++ src/app/appconfiguration.service.ts
@@ -7,6 +7,9 @@ export class AppconfigurationService {
   baseUrl: string;
 
   constructor() {
+    // git update-index --assume-unchanged src/app/appconfiguration.service.ts
+    // -> git won't track this file, --no-assume-unchanged to revert
+
     // base-url for php-backend
     this.baseUrl = 'http://localhost/';
    }
